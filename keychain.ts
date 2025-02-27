341f08b4a847132286667521dd06e241580b7f1f
Dmitry <d@kozlov.ru>
2025-02-27 22:33:33 +0700

    Added keychain for connection signing

---
+++ keychain.ts
@@ -0,0 +1,42 @@
+import crypto from 'crypto'
+
+export async function sign(expires: number = 10) {
+  const key = await loadKey()
+  const publicKey = getPublicKey(key)
+  const data = `${Math.round(new Date().getTime() / 1000) + expires}`
+  const signature = makeSignature(key, data)
+  return `key.${publicKey}.${signature}.${data}`
+}
+
+async function loadKey(): Promise<string> {
+  try {
+    return await Bun.file('auth').text()
+  } catch {
+    const key = await generateKey()
+    await Bun.file('auth').write(key)
+    return key
+  }
+}
+async function generateKey(): Promise<string> {
+  const keys = crypto.generateKeyPairSync('ed25519', {
+    privateKeyEncoding: { format: 'der', type: 'pkcs8' },
+    publicKeyEncoding: { format: 'der', type: 'spki' },
+  })
+  return Buffer.concat([keys.privateKey, keys.publicKey]).toString('base64')
+}
+function getPublicKey(key: string): string {
+  return Buffer.from(key, 'base64')
+    .subarray(48, 48 + 44)
+    .toString('base64')
+}
+function makeSignature(key: string, data: string): string {
+  const keyBuffer = Buffer.from(key, 'base64')
+  const privateKey = crypto.createPrivateKey({
+    key: keyBuffer.subarray(0, 48), // Extract private key
+    format: 'der',
+    type: 'pkcs8',
+  })
+
+  const signature = crypto.sign('sha256', Buffer.from(data), privateKey)
+  return signature.toString('base64')
+}
