c765e63d6562c557107bf4923f2bd73adb403a61
Dmitry <d@kozlov.ru>
2025-07-10 10:32:55 +0300

    Added App

---
+++ app.ts
@@ -0,0 +1,77 @@
+export class App {
+  header: AppHeader
+  body: AnyElement[]
+  data: Record<string, string>
+  constructor(header: AppHeader, body: AnyElement[], data: Record<string, string> = {}) {
+    this.header = header
+    this.body = body
+    this.data = data
+  }
+}
+
+interface AppHeader {
+  name: string
+  path: string
+}
+
+type AnyElement = string | TextElement | TextFieldElement | ButtonElement | ListElement | PickerElement | CellElement
+interface TextElement {
+  type: 'text'
+  value: string
+  secondary?: boolean
+}
+interface TextFieldElement {
+  type: 'textField'
+  value: string
+  placeholder?: string
+  action?: Action
+}
+interface ButtonElement {
+  type: 'button'
+  title: string
+  action: Action
+}
+interface ListElement {
+  type: 'list'
+  data: string
+  cell: AnyElement
+}
+interface PickerElement {
+  type: 'picker'
+  options: string[]
+  selected: string
+}
+interface CellElement {
+  type: 'cell'
+  title: AnyElement
+  subtitle?: AnyElement
+}
+interface Action {
+  path: string
+  body?: string | Record<string, string>
+  output?: string | Record<string, string>
+}
+
+export function textField(value: string, options: { placeholder?: string; action?: Action }): TextFieldElement {
+  return {
+    type: 'textField',
+    value,
+    placeholder: options.placeholder,
+    action: options.action,
+  }
+}
+export function text(value: string): string {
+  return value
+}
+export function button(title: string, action: Action): ButtonElement {
+  return { type: 'button', title, action }
+}
+export function list(data: string, cell: AnyElement): ListElement {
+  return { type: 'list', data, cell }
+}
+export function picker(options: string[], selected: string): PickerElement {
+  return { type: 'picker', options, selected }
+}
+export function cell(title: AnyElement, subtitle: AnyElement): CellElement {
+  return { type: 'cell', title, subtitle }
+}
