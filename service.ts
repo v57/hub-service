cf81523e663d7f8f12f9975560779f7fe6e4fa82
Dmitry <d@kozlov.ru>
2025-11-09 19:26:29 +0700

    Service: Added api settings

--- service.ts
+++ service.ts
@@ -7,30 +7,69 @@ export class Service {
   address: string | number
   channel = new Channel()
   apps: AppHeader[] = []
+  group?: string
+  settings: Record<string, ApiSettings> = {}
   constructor(address?: string | number) {
     this.address = address ?? Bun.env.HUB ?? 1997
   }
   start() {
-    const api = [...Object.keys(this.channel.postApi.storage), ...Object.keys(this.channel.streamApi.storage)]
+    const services: ServiceHeader[] = []
+    for (const path of Object.keys(this.channel.postApi.storage)) {
+      services.push({
+        path,
+        settings: this.settings[path],
+      })
+    }
+    for (const path of Object.keys(this.channel.streamApi.storage)) {
+      services.push({
+        path,
+        settings: this.settings[path],
+      })
+    }
     const apps = this.apps
     this.channel.connect(this.address, {
       headers: async () => ({ auth: await sign(), v }),
       async onConnect(sender) {
-        await sender.send('hub/service/update', { add: api, addApps: apps })
+        await sender.send('hub/service/update', { services, apps })
       },
     })
   }
-  post(path: string, action: (body: any) => any | Promise<any>) {
+  post(path: string, action: (body: any) => any | Promise<any>, settings?: ApiSettings) {
+    this.addSettings(path, settings)
     this.channel.post(path, ({ body }) => action(body))
     return this
   }
-  stream(path: string, action: (body: any) => AnyIterator) {
+  stream(path: string, action: (body: any) => AnyIterator, settings?: ApiSettings) {
+    this.addSettings(path, settings)
     this.channel.stream(path, ({ body }) => action(body))
     return this
+  }
+  private addSettings(path: string, settings: ApiSettings | undefined) {
+    if (!settings) return
+    this.settings[path] = settings
+    if (settings.permissions && !settings.permissions.group) {
+      if (!this.group) this.group = path.split('/')[0]
+      settings.permissions.group = this.group
+    }
   }
 }
 export interface AppHeader {
   type: 'app'
   name: string
   path: string
+}
+export interface ApiSettings {
+  permissions?: ServicePermissions
+}
+export interface ServicePermissions {
+  group?: string
+  /// Permission name
+  name: string
+  /// Allows permission by default
+  allows?: boolean
+}
+
+interface ServiceHeader {
+  path: string
+  settings: ApiSettings
 }
